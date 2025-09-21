066353110c0197e1c1f13df660617de83ad8273f
Mustafa <mustafa.mohamed2021b@gmail.com>
2025-09-21 10:55:42 +0300

    remove unused packages and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster } from "sonner";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
